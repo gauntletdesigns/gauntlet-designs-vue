cf1a227f99bec7417328473ed5921e27b82088ce
David Gauntlett <david.m.gauntlett@gmail.com>
2022-07-24 00:37:45 -0400

    basic about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,9 @@
   <b-container class="pt-5">
     <h2>About Us</h2>
     <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid aperiam, consectetur eaque, esse eum incidunt
-      iure iusto laudantium, natus nemo nesciunt omnis praesentium quaerat qui quisquam rerum sapiente voluptatibus.
+      Gauntlet Designs, a female owned business, has a desire to make the web a better place. We strive to make
+      incredible looking websites that function as well as they look. We have a strong belief that modern websites can
+      both look good, and be usable by everyone despite any unique challenges an individual might face.
     </div>
   </b-container>
 </template>
