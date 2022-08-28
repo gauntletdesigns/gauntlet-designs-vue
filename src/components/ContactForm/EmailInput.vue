4cd44dad48a56f021c6903b53d8813cb21687475
afton <afton.gauntlett@gmail.com>
2022-08-28 13:23:51 -0400

    wip

--- src/components/ContactForm/EmailInput.vue
+++ src/components/ContactForm/EmailInput.vue
@@ -1,24 +1,25 @@
 <template>
   <b-form-group
-      id="input-email"
-      label="Email*"
-      label-for="input-email"
+    id="input-email"
+    label="Email*"
+    label-for="input-email"
+    description="We'll never share your email address."
   >
     <b-form-input
-        id="input-email"
-        v-model="form.email"
-        type="email"
-        required
-        placeholder="Email"
+      id="input-email"
+      v-model="form.email"
+      type="email"
+      required
+      placeholder="Email"
     >
     </b-form-input>
   </b-form-group>
 </template>
 <script>
 export default {
-  name: 'EmailInput',
+  name: "EmailInput",
   props: {
-    form: {}
-  }
-}
+    form: {},
+  },
+};
 </script>
