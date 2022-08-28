58430cc45e0a78c411f4e54ac58112f2927778df
afton <afton.gauntlett@gmail.com>
2022-08-28 18:57:57 -0400

    scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-import routes from './routes';
+import routes from "./routes";
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { el: to.hash, behavior: "smooth" };
+    } else {
+      window.scrollTo(0, 0);
+    }
+  },
+  routes,
+});
 
-export default router
+export default router;
