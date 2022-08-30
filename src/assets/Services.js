fef13b6942bdf61455cbde1511462ea89a01b11f
afton <afton.gauntlett@gmail.com>
2022-08-29 20:25:37 -0400

    adding meta data and cloud logo

--- src/assets/Services.js
+++ src/assets/Services.js
@@ -1,36 +1,63 @@
 export default [
-    {
-        name: 'Web Design',
-        paragraphs: [
-            'Our talented team of designers will design a custom website for your business. Our designers will work with you to ensure that your website feels unique, and matches your company branding.',
-            'Our designs take best modern web practices in layout, accessibility, and responsive design into account. So you can trust that your all of your clients get the same tailored experience.'
-        ]
-    },
-    {
-        name: 'Web Development',
-        paragraphs: [
-            'A great design doesn\'t go far without great developers. Our team of developers will produce the website that you have designed with our design team. Already have a design? We can build that for you as well.',
-            'Your website will be built using modern web tools and frameworks. Maintenance and security are baked in, so any updates you need in the future can be applied as quickly as possible.'
-        ]
-    },
-    {
-        name: 'Hosting',
-        paragraphs: [
-            'We will host your website in the cloud in order to provide the best possible uptime, and fastest possible speeds.',
-            'We can either purchase your desired domain (pending availability) on your behalf, you will help you connect your existing web address to your new website.'
-        ]
-    },
-    {
-        name: 'Maintenance',
-        paragraphs: [
-            'Our team will manage security updates, and any content updates that you may need. This means you will not need tow worry about the upkeep of your website and it will "just work".'
-        ]
-    },
-    {
-        name: 'Consulting',
-        paragraphs: [
-            'Already have an existing application, but need some surge support to meet a deadline? Let us help. Our experienced team of developers and designers can help you solve the unique problems your business is tackling.',
-            'We can provide support for the full stack of web development, for any of the three major clouds (Google, Azure, AWS).'
-        ]
-    }
-]
+  {
+    name: "Web Design",
+    paragraphs: [
+      "Our talented team of designers will work with you to ensure that your website feels unique, and matches your" +
+        " company branding. Already have a website or design that needs updating? We can help with that, too.",
+      "Our designs take best modern web practices in layout, accessibility, and responsive design into account. So" +
+        " you can trust that all of your clients get the same tailored experience. We can also help with simple" +
+        " logos and brand packaging if needed.",
+    ],
+  },
+  {
+    name: "Web Development",
+    paragraphs: [
+      "A great design doesn't go far without great developers. Our team of developers will take your finalized" +
+        " design and turn it into a fast and responsive website. Your website will be built using top modern web" +
+        " tools and frameworks.",
+      "Our websites are built 100% in-house, we do not use website builders or pre-built templates. This means your" +
+        " website will be unique, fast, secure and fully customizable to meet your needs",
+    ],
+  },
+  {
+    name: "Hosting",
+    paragraphs: [
+      "We utilize the reliability and reach of Google Cloud to provide the best possible uptime, and fastest" +
+        " possible speeds for our hosting. Google's data cloud is unmatched in terms of speed, scale, and security.",
+      "We can either purchase your desired domain (pending availability) on your behalf, or we can help you" +
+        " connect your existing web address to your new website.",
+    ],
+  },
+  {
+    name: "Maintenance",
+    paragraphs: [
+      "We provide monthly content updates, so you will not need to worry about the upkeep of your" +
+        " website. These updates include basic changes such as adding or removing images, updating text areas," +
+        " links, icons and logos on your website. All you have to do is provide us the content updates and we handle" +
+        " the rest!",
+      "We perform monthly patches and security" +
+        " updates on every site each month, keeping you protected. Because we proudly use Google Cloud, rest easy" +
+        " knowing" +
+        " Google" +
+        " keeps more people safe" +
+        " online than anyone else in the world.",
+    ],
+  },
+  {
+    name: "SEO",
+    paragraphs: [
+      "We optimize the content, structure, and layout of our websites to achieve the best ranking results in search" +
+        " engines.",
+      "Our websites are Section 508 Compliant, meaning they are fully accessible for all users. Our" +
+        " website feature text alternatives, fonts with increased readability, closed captioning in video posts," +
+        " scripting workarounds to accommodate some assistive technologies and color alternatives",
+    ],
+  },
+  {
+    name: "Consulting",
+    paragraphs: [
+      "Already have an existing application, but need some surge support to meet a deadline? Let us help. Our experienced team of developers and designers can help you solve the unique problems your business is tackling.",
+      "We can provide support for the full stack of web development, for any of the three major clouds (Google, Azure, AWS).",
+    ],
+  },
+];
