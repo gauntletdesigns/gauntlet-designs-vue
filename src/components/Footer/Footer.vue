a19928040f2dfe12f342d00b83a0ebf8ece1c1f5
afton <afton.gauntlett@gmail.com>
2022-08-30 23:22:04 -0400

    making the footer smaller

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="color-background w-100 h-100">
-    <b-row class="footer text">
+    <b-row class="footer">
       <b-col
         cols="12"
         md="3"
         lg="2"
         offset-lg="1"
         offset-xl="2"
-        class="d-flex flex-column align-items-center"
+        class="justify-content-center d-none d-md-flex"
       >
-        <p>571-265-2903</p>
-        <p>
-          <a class="email" href="mailto:afton@gauntletdesigns.com">Email Us</a>
-        </p>
-        <p>Herndon, VA</p>
+        <div class="address">
+          <div>571-265-2903</div>
+          <div>
+            <a class="email" href="mailto:afton@gauntletdesigns.com"
+              >Email Us</a
+            >
+          </div>
+          <div>Herndon, VA</div>
+        </div>
       </b-col>
 
       <b-col
@@ -32,20 +36,19 @@
         cols="12"
         lg="2"
         md="3"
-        class="d-flex flex-column align-items-center mt-3 mt-md-0 justify-content-center"
+        class="d-flex flex-column align-items-center mt-md-0 justify-content-center"
       >
         <b-button
-          class="contact-button mb-3"
+          class="contact-button"
           variant="success"
-          size="lg"
+          size="md"
           :to="{ name: 'contact' }"
           >Contact Us</b-button
         >
       </b-col>
       <b-col cols="12" class="d-flex justify-content-center">
-        <p class="copyright-text">
-          &copy; Copyright {{ new Date().getFullYear() }} built by Gauntlet
-          Designs
+        <p class="copyright-text pt-3">
+          &copy; Copyright Gauntlet Designs {{ new Date().getFullYear() }}
         </p>
       </b-col>
     </b-row>
@@ -65,12 +68,8 @@ export default {
 <style scoped lang="scss">
 .footer {
   background-color: $secondary-transparent;
-  min-height: 50px;
   color: $light-text;
-  padding-top: 50px;
-  padding-left: 50px;
-  padding-right: 50px;
-
+  padding-top: 20px;
   // 0 px margin to fix default row spacing causing horizontal scroll bar
   margin-right: 0px !important;
   margin-left: 0px !important;
@@ -80,17 +79,9 @@ export default {
   background-color: $secondary-transparent;
 }
 
-.copyright {
-  padding-top: 20px;
-  font-size: 14px;
-}
-
-.copyright-container {
-  background-color: $secondary-transparent;
-}
-
 .copyright-text {
   color: #aeaeb0;
+  font-size: 12px;
 }
 
 .email {
@@ -107,6 +98,7 @@ export default {
   font-family: "Montserrat", sans-serif;
   letter-spacing: 4px;
   font-weight: 600;
+  padding: 10px;
 }
 
 .logo-button {
