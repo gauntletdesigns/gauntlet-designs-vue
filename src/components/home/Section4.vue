fa0dc600f6a187769ad261c5220d40cfc46c05e6
David Gauntlett <david.m.gauntlett@gmail.com>
2022-08-30 21:58:59 -0400

    small tweak to include enterprise grade applications

--- src/components/home/Section4.vue
+++ src/components/home/Section4.vue
@@ -3,11 +3,12 @@
     <b-container class="d-flex flex-column align-items-left">
       <h2 class="sub-title">Welcome</h2>
       <p class="text">
-        Welcome to Gauntlet Designs. We design, build, and host custom websites
+        Welcome to Gauntlet Designs. We design, build, and host custom websites and enterprise grade applications
         to fit your needs. Be that a small family owned business, or a
         enterprise looking to expand. We are a female owned business located in
         the DC Metro Area. Our products are clean, light, responsive and fully
         customizable to meet your business needs.
+        <b-btn :to="{name: 'services'}" variant="link" class="py-0 learn-more">Learn more...</b-btn>
       </p>
     </b-container>
   </div>
@@ -23,5 +24,10 @@ export default {
 .outer {
   background-color: $background;
   padding: 60px 0px 30px 0px;
+}
+
+.learn-more {
+  color: $accent;
+  font-weight: 600;
 }
 </style>
