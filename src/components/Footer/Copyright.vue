6204d5de82d0d5a519a27ec6d4d9d43330f6d4ad
David Gauntlett <david.m.gauntlett@gmail.com>
2022-09-05 11:19:16 -0600

    fixing footer

---
+++ src/components/Footer/Copyright.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-col cols="12" class="d-flex justify-content-center">
+    <p class="copyright-text pt-3">
+      &copy;Copyright Gauntlet Designs {{ new Date().getFullYear() }}
+    </p>
+  </b-col>
+</template>
+<script>
+export default {
+  name: 'Copyright'
+}
+</script>
+<style scoped lang="scss">
+
+.copyright-text {
+  color: #e2e2e8;
+  font-size: 12px;
+}
+
+</style>
